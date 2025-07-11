dc88abc4a8a582429f23c92ed22ee09375b32d09
kimtaegun <tee1694@naver.com>
2025-07-11 13:43:30 +0900

    test1

--- backend/mallapi/src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-//
+//11
 @RestController
 @RequiredArgsConstructor
 @Log4j2
